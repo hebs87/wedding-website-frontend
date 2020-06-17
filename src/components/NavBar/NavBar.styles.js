cbc2fce69df1ccfd2f387f41ae11d933fc56554a
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-17 21:49:10 +0100

    Applied fixed positioning to NavBar

--- src/components/NavBar/NavBar.styles.js
+++ src/components/NavBar/NavBar.styles.js
@@ -3,6 +3,9 @@ import {makeStyles} from "@material-ui/core/styles";
 export const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    width: '100%',
+    position: 'fixed',
+    top: 0,
   },
   navBar: {
     backgroundColor: 'transparent',

11eebf75b452c029f335e45b776fb38494d2e12c
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-17 20:32:06 +0100

    Moved NavBar styles into separate file

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,53 +1,10 @@
-import {makeStyles} from "@material-ui/core/styles";
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 // import {NavLink} from "react-router-dom";
-
-export const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  navBar: {
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-  },
-  toolbarRoot: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    paddingTop: 10,
-    paddingBottom: 10,
-    [theme.breakpoints.down('xs')]: {
-      paddingRight: 0,
-      paddingLeft: 0,
-    },
-  },
-  logoLink: {
-    marginRight: '0',
-    color: '#b3b3b3',
-    [theme.breakpoints.up('sm')]: {
-      marginRight: '10px',
-    },
-    '&:hover': {
-      background: 'transparent',
-    },
-  },
-  navLink: {
-    margin: '0 2px',
-    color: '#b3b3b3',
-    padding: '10px',
-    textDecoration: 'none',
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-    transition: 'all 0.35s ease-in-out',
-    "&:hover": {
-      backgroundColor: 'transparent',
-      color: '#d83600',
-    },
-  },
-}));
+import {useStyles} from './NavBar.styles';
 
 const NavBar = () => {
   // Get the classes from the useStyles function
---
+++ src/components/NavBar/NavBar.styles.js
@@ -0,0 +1,44 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  navBar: {
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+  },
+  toolbarRoot: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    paddingBottom: 10,
+    [theme.breakpoints.down('xs')]: {
+      paddingRight: 0,
+      paddingLeft: 0,
+    },
+  },
+  logoLink: {
+    marginRight: '0',
+    color: '#b3b3b3',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: '10px',
+    },
+    '&:hover': {
+      background: 'transparent',
+    },
+  },
+  navLink: {
+    margin: '0 2px',
+    color: '#b3b3b3',
+    padding: '10px',
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    transition: 'all 0.35s ease-in-out',
+    "&:hover": {
+      backgroundColor: 'transparent',
+      color: '#d83600',
+    },
+  },
+}));

617cd7c82b443aa1cb123f880482a7bafe208d77
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-18 20:24:26 +0100

    Removed Banner from App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import './App.css';
 import NavBarContainer from "./components/NavBar/NavBarContainer";
-import Banner from "./components/Banner/Banner";
-import OrangeTree from './static/assets/orange-tree.jpg';
 
 const App = () => {
   return (
     <div className="App">
       <NavBarContainer/>
-      <Banner
-        imageUrl={OrangeTree}
-        title={'Join us for our Sunshine State Wedding'}
-        firstTextLine={'1 June, 2021'}
-        secondTextLine={'Orlando, Florida'}
-        button={'RSVP'}
-      />
     </div>
   );
 }

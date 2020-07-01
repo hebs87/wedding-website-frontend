f0e798285106a829404d5e741af5bf89147f2483
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-01 23:29:52 +0100

    Created custom scroll handler to apply custom styles to mobile navbar on scroll

--- src/components/NavBar/MobileNavBar/MobileNavBar.jsx
+++ src/components/NavBar/MobileNavBar/MobileNavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useScrollHandler} from "../../../hooks/ScrollHook";
 import {NavLink} from 'react-router-dom';
 import styled from 'styled-components';
 import Burger from './Burger';
@@ -6,12 +7,13 @@ import Burger from './Burger';
 const Nav = styled.nav`
   width: 100%;
   height: 55px;
-  padding: 0 20px;
+  padding: ${({scroll}) => scroll ? '0 20px' : '0 20px 10px'};
   display: flex;
   justify-content: space-between;
   position: fixed;
   align-items: center;
   color: #b3b3b3;
+  background-color: ${({scroll}) => scroll ? 'transparent' : 'rgba(0, 0, 0, 0.8)'};
   a {
     text-decoration: none;
   }
@@ -31,8 +33,10 @@ const Nav = styled.nav`
 `
 
 const MobileNavBar = () => {
+  const scroll = useScrollHandler(20);
+
   return (
-    <Nav>
+    <Nav scroll={scroll}>
       <div className="logo">
         <NavLink
           exact to="/"

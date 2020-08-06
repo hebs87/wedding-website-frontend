219e2f1cdfac10bd5e762a32480c763b762ee4eb
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-08-06 21:55:23 +0100

    Imported Error into App and rendered if hasError is true

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
 import NavBarContainer from "./components/NavBar/NavBarContainer";
@@ -6,6 +6,7 @@ import WeddingPage from "./pages/Wedding/Wedding";
 import Location from "./pages/Location/Location";
 import RSVP from "./pages/RSVP/RSVP";
 import Footer from "./components/Footer/Footer";
+import Error from "./components/Error/Error";
 
 class App extends Component {
   constructor(props) {
@@ -14,29 +15,41 @@ class App extends Component {
     this.state = {
       showFlashMessage: false,
       attending: false,
+      hasError: true,
     }
   }
 
   render() {
+    const {hasError} = this.state;
+
     return (
       <div className="App">
-        <NavBarContainer/>
-        <Route
-          exact
-          path="/"
-          component={WeddingPage}
-        />
-        <Route
-          exact
-          path="/location"
-          component={Location}
-        />
-        <Route
-          exact
-          path="/rsvp"
-          component={RSVP}
-        />
-        <Footer/>
+        {
+          hasError &&
+          <Error/>
+        }
+        {
+          !hasError &&
+          <Fragment>
+            <NavBarContainer/>
+            <Route
+              exact
+              path="/"
+              component={WeddingPage}
+            />
+            <Route
+              exact
+              path="/location"
+              component={Location}
+            />
+            <Route
+              exact
+              path="/rsvp"
+              component={RSVP}
+            />
+            <Footer/>
+          </Fragment>
+        }
       </div>
     );
   }

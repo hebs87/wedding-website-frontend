87092ae6b9cda60e0f7cf35344106eb91d609fa5
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2025-01-24 16:49:06 +0000

    Feature/configure theme (#2)
    * Removed redundant boilerplate Vite files, and configured the following:
        - Theme colours
        - Breakpoint definitions
        - Fonts
        - Favicon
    
    Co-authored-by: sunny <sunny@jbcole.co.uk>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Cooper: ['Cooper', 'sans-serif'],
+        Amiko: ['Amiko', 'sans-serif'],
+        AmikoSemiBold: ['AmikoSemiBold', 'sans-serif'],
+        AmikoBold: ['AmikoBold', 'sans-serif'],
+      },
+      colors: {
+        background: '#F8F8F8',
+        title: '#C80100',
+        text: '#8C1F1E',
+      },
+      screens: {
+        xs: '480px',
+        sm: '600px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
   },
   plugins: [],
 };

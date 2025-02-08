f5e3c380938ace42229ad7059f168615ef6ca1f6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2025-02-08 10:55:31 +0000

    Made pre-live copy updates and increased page container mobile padding bottom (#33)
    Co-authored-by: sunny <sunny@jbcole.co.uk>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,13 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div data-testid="Footer" className="Footer flex h-[40px] w-full items-center justify-center text-[12px]">
-      &copy; {currentYear} Sunny Hebbar
+    <div
+      data-testid="Footer"
+      className="Footer flex h-[60px] w-full items-center justify-center text-center text-[12px]"
+    >
+      &copy; {currentYear}
+      <br />
+      Designed & created by Sunny & Kim
     </div>
   );
 };

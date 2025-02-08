2359ccf9840bcc9d21a1b6e74867e3828f405376
sunny <sunny@jbcole.co.uk>
2025-02-08 00:39:15 +0000

    Removed redundant /gallery path from codebase

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -2,6 +2,5 @@ export const PATH_HOME = '/';
 export const PATH_OUR_STORY = '/our-story';
 export const PATH_WEDDING = '/wedding';
 export const PATH_PARTY = '/party';
-export const PATH_GALLERY = '/gallery';
 export const PATH_RSVP = '/rsvp';
 export const CODE_PARAM = ':code';
